6a288c95d381629113557e57f4a13b7d136b5f51
Bronislav “Bronislav” <89222790+SoTokcuk@users.noreply.github.com>
2023-03-14 06:17:18 +0000

    feat: add is_prime function

--- src/main_server.c
+++ src/main_server.c
@@ -2,3 +2,18 @@
 //Created by Branislau Yanchuk 14.03.2023
 
 //Here you can find an implementation of the functions from main_interface.h
+
+#include "main_interface.h"
+
+int is_prime(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
