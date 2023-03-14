bc4dbb7ad33972d1ed5eb7ccb7f0bfddff1b1a52
Bronislav “Bronislav” <89222790+SoTokcuk@users.noreply.github.com>
2023-03-14 06:25:40 +0000

    feat: add find_primes function doc: add the explanation

--- .ccls-cache/@home@runner@lab3-task1-gr13-yanchuk/src@main_interface.h
+++ .ccls-cache/@home@runner@lab3-task1-gr13-yanchuk/src@main_interface.h
@@ -1,2 +1,9 @@
 //
 // Created by Branislau Yanchuk 14.03.2023
+
+// find_primes
+// This function goes through all numbers from 100 to 999 
+// and calls the is_prime function for each number. 
+// If the number is prime, it is displayed with the printf function.
+
+void find_primes();
